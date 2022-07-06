8b7e1c7acd54c773e5a6143840faaefb1e410c5d
hp <adityashinde8855@gmail.com>
2022-07-06 19:47:29 +0530

    My first java prg

---
+++ FebonnaciSeries.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class FebonnaciSeries{
+	
+	public static void main(String[] args){
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter a no.");
+		
+		int num= sc.nextInt();
+	
+	int a=0;
+	int b=1;
+	
+	int c;
+	while(num>=0){
+		
+		c=a+b;
+		System.out.println(a);
+		a=b;
+		b=c;
+		
+		
+		num--;
+	}
+	
+	
+	
+	}
+	
+	
+}
+
+
