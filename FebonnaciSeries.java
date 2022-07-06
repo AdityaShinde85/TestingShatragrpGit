ce37b3d7ca4f5b5104302c917bcfd306048913e0
hp <adityashinde8855@gmail.com>
2022-07-07 00:11:13 +0530

    done the changes

--- FebonnaciSeries.java
+++ FebonnaciSeries.java
@@ -10,15 +10,15 @@ public class FebonnaciSeries{
 		
 		int num= sc.nextInt();
 	
-	int a=0;
+	int r=0;
 	int b=1;
 	
 	int c;
 	 while(num>=0){
 		
-		c=a+b;
-		System.out.println(a);
-		a=b;
+		c=r+b;
+		System.out.println(r);
+		r=b;
 		b=c;
 		
 		
